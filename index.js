a7ff58fedd769b73ac1a1e419d49c51322c1b7b7
Ankush Rajput <141709546+ankushsingh8416@users.noreply.github.com>
2024-09-21 08:43:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+app.get('/',  (req, res) => {
+res.send("Hello markly ")
+            })
 // User Registration Route
 app.post('/register', async (req, res) => {
     const { username, email, password } = req.body;
